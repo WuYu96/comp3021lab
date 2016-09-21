359bbe309a254b9d6e3556ad6eb1e53031353d4c
WuYu96 <ywucg@connect.ust.hk>
2016-09-21 19:52:11 +0800

    Update Folder.java

--- src/base/Folder.java
+++ src/base/Folder.java
@@ -1,25 +1,50 @@
 package base;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 
-public class Folder {
+public class Folder implements Comparable<Folder>{
 	private ArrayList<Note> notes;
 	private String name;
-    public Folder(String name){
-    	this.name = name;
-    	notes = new ArrayList<Note>(); 
-    }
+	
+	public int compareTo(Folder o){
+		
+		if(this.name.length()==o.name.length()){
+			return 0;
+		}
+		else if(this.name.length()>o.name.length()){
+			return 1;
+		}
+		else{
+			return -1;
+		}
+	}
+	public List<Note> searchNotes(String keywords){ 
+		String[] words = keywords.split(" ");
+	}
+	
+	public void sortNotes(){
+		List<Note> lnotes = notes;
+		Collections.sort(notes);
+		
+	}
+        public Folder(String name){
+    		this.name = name;
+    		notes = new ArrayList<Note>(); 
+        }
     	
-    public void addNote(Note note){
-    	notes.add(note);
-    }
-    public String getName(){
-    	return name;
-    }
-    public ArrayList<Note> getNotes(){
-    	return notes;
-    }
+        public void addNote(Note note){
+    		notes.add(note);
+        }
+    	public String getName(){
+    		return name;
+    	}
+    	public ArrayList<Note> getNotes(){
+    		return notes;
+    	}
     
+    	@Override
 	public String toString() {
 	    int nText = 0;
 	    int nImage = 0;
@@ -37,7 +62,7 @@ public class Folder {
 	    	return name.equals(other.getName());
 	    }
 	    
-@Override
+	@Override
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
