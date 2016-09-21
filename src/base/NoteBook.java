759207f8ef859a720d1ff2b659280c057a0e3108
WuYu96 <ywucg@connect.ust.hk>
2016-09-21 19:55:30 +0800

    Update NoteBook.java

--- src/base/NoteBook.java
+++ src/base/NoteBook.java
@@ -1,33 +1,58 @@
-package base;
+package base_local;
+
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class NoteBook {
+
 	private ArrayList<Folder> folders;
-	public NoteBook(){
+	
+	public void sortFolders(){
+		
+		for(Folder f : folders){
+			f.sortNotes();
+		}
+		
+		List<Folder> lfolders = folders;
+		Collections.sort(folders);
+		
+	}
+
+	public NoteBook() {
+
 		folders = new ArrayList<>();
 	}
-	public boolean createTextNote(String folderName, String title){
+	
+	public ArrayList<Folder> getFolders(){
+		
+		return folders;
+		
+	}
+
+	public boolean createTextNote(String folderName, String title) {
 		TextNote note = new TextNote(title);
-		return insertNote(folderName,note);
+		return insertNote(folderName, note);
 	}
-	public boolean createImageNote(String folderName, String title){
-	    ImageNote note = new ImageNote(title);
-	    return insertNote(folderName,note);
+
+	public boolean createImageNote(String folderName, String title) {
+		ImageNote note = new ImageNote(title);
+		return insertNote(folderName, note);
 	}
-	private boolean insertNote(String folderName,Note note){
+
+	private boolean insertNote(String folderName, Note note) {
 		Folder f = null;
-		for(Folder f1:folders){
-			if(f1.getName().equals(folderName)){
-				f=f1;
+		for (Folder f1 : folders) {
+			if (f1.getName().equals(folderName)) {
+				f = f1;
 				break;
 			}
 		}
-		if(f == null){
+		if (f == null) {
 			f = new Folder(folderName);
 			folders.add(f);
 		}
-		for(Note n:f.getNotes()){
-			if(n.getTitle().equals(note.getTitle())){
+		for (Note n : f.getNotes()) {
+			if (n.getTitle().equals(note.getTitle())) {
 				System.out.println("Creating note" + note.getTitle() + "under folder" + folderName + "failed");
 				return false;
 			}
@@ -36,5 +61,3 @@ public class NoteBook {
 		return true;
 	}
 }
-
-
