2f599d217dc4b5c2cb942114ec2c04d5d90bf46c
WuYu96 <ywucg@connect.ust.hk>
2016-09-21 19:56:29 +0800

    Update ImageNote.java

--- src/base/ImageNote.java
+++ src/base/ImageNote.java
@@ -1,12 +1,14 @@
-package base;
+package base_local;
 import java.io.File;
+import java.util.List;
 
 public class ImageNote extends Note {
 	File image;
 	public ImageNote(String title){
 		super(title);
 	}
-	public void ImageNote(){
-	}
+	
+	
+	
 
 }
